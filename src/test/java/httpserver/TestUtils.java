52629b23563c2fed613dd26ea767ca035d77035a
Klaven Jones <klavensjones@gmail.com>
2022-08-10 19:02:44 -0400

    Added Test Utils

---
+++ src/test/java/httpserver/TestUtils.java
@@ -0,0 +1,36 @@
+package httpserver;
+
+import static httpserver.constants.HTTPLines.CRLF;
+
+public class TestUtils {
+
+    public static String dummyRequestData() {
+        return "POST /echo_body HTTP/1.1 \n" +
+                "Connection: close\n" +
+                "Host: 127.0.0.1:5000\n" +
+                "User-Agent: http.rb/4.3.0\n" +
+                "Content-Length: 9\r\n" +
+                "\r\n" +
+                "some body";
+    }
+
+    public static String dummyEchoData() {
+        return "GET /simple_get HTTP/1.1" + CRLF +
+                "Connection: close" + CRLF +
+                "Host: 127.0.0.1:5000" + CRLF +
+                "User-Agent: http.rb/4.3.0" + CRLF +
+                "Content-Length: 9" + CRLF + CRLF +
+                "some body";
+    }
+
+    public static String dummy404Data() {
+        return "GET /simple_non_existent HTTP/1.1" + CRLF +
+                "Connection: close" + CRLF +
+                "Host: 127.0.0.1:5000" + CRLF +
+                "User-Agent: http.rb/4.3.0" + CRLF +
+                "Content-Length: 9" + CRLF + CRLF +
+                "some body";
+    }
+
+}
+
