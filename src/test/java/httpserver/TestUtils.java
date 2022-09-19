fbe1c092a55116960d63b2ff4c365c1c69edc96a
Klaven Jones <klavensjones@gmail.com>
2022-09-19 11:31:48 -0400

    Fixed linting issues

--- src/test/java/httpserver/TestUtils.java
+++ src/test/java/httpserver/TestUtils.java
@@ -12,64 +12,53 @@ import static httpserver.constants.StatusCode.OK;
 public class TestUtils {
 
     public static String mockRequestData() {
-        return "POST /echo_body HTTP/1.1 \n" +
-                "Connection: close\n" +
-                "Host: 127.0.0.1:5000\n" +
-                "User-Agent: http.rb/4.3.0\n" +
-                "Content-Length: 9\r\n" +
-                "\r\n" +
-                "some body";
+        return "POST /echo_body HTTP/1.1 \n" + "Connection: close\n" +
+                "Host: 127.0.0.1:5000\n" + "User-Agent: http.rb/4.3.0\n" +
+                "Content-Length: 9\r\n" + "\r\n" + "some body";
     }
 
     public static String mockEchoData() {
-        return "GET /simple_get HTTP/1.1" + CRLF +
-                "Connection: close" + CRLF +
-                "Host: 127.0.0.1:5000" + CRLF +
-                "User-Agent: http.rb/4.3.0" + CRLF +
-                "Content-Length: 9" + CRLF + CRLF +
-                "some body";
+        return "GET /simple_get HTTP/1.1" + CRLF + "Connection: close" + CRLF +
+                "Host: 127.0.0.1:5000" + CRLF + "User-Agent: http.rb/4.3.0" +
+                CRLF + "Content-Length: 9" + CRLF + CRLF + "some body";
     }
 
     public static String mock404Data() {
         return "GET /simple_non_existent HTTP/1.1" + CRLF +
-                "Connection: close" + CRLF +
-                "Host: 127.0.0.1:5000" + CRLF +
-                "User-Agent: http.rb/4.3.0" + CRLF +
-                "Content-Length: 9" + CRLF + CRLF +
-                "some body";
+                "Connection: close" + CRLF + "Host: 127.0.0.1:5000" + CRLF +
+                "User-Agent: http.rb/4.3.0" + CRLF + "Content-Length: 9" +
+                CRLF + CRLF + "some body";
     }
 
     public static String mockGetData(String pathName) {
         return "GET" + SP + pathName + SP + "HTTP/1.1\n" +
-                "Connection: close\n" +
-                "Host: 127.0.0.1:5000\n" +
-                "User-Agent: http.rb/4.3.0\n" +
-                "Content-Length: 0" + CRLF + CRLF;
+                "Connection: close\n" + "Host: 127.0.0.1:5000\n" +
+                "User-Agent: http.rb/4.3.0\n" + "Content-Length: 0" + CRLF +
+                CRLF;
     }
 
     public static String mockResponse() {
-        return DEFAULT_VERSION + SP + OK.code + CRLF
-                + "Allow: GET, HEAD, OPTIONS, PUT, POST" + CRLF
-                + "Content-Length: 9" + CRLF + CRLF
-                + "some body";
+        return DEFAULT_VERSION + SP + OK.code + CRLF +
+                "Allow: GET, HEAD, OPTIONS, PUT, POST" + CRLF +
+                "Content-Length: 9" + CRLF + CRLF + "some body";
     }
 
     public static String mockResponseWithContentLength(String length) {
-        return DEFAULT_VERSION + SP + OK.code + CRLF
-                + "Content-Length:" + SP + length + CRLF + CRLF
-                + "";
+        return DEFAULT_VERSION + SP + OK.code + CRLF + "Content-Length:" + SP +
+                length + CRLF + CRLF + "";
     }
 
     public static String mockResponseWithMultipleHeaders(String length,
                                                          String body) {
-        return DEFAULT_VERSION + SP + OK.code + CRLF
-                + "Content-Length:" + SP + length + CRLF
-                + "Allow:" + SP + "GET, HEAD, OPTIONS" + CRLF + CRLF
-                + body;
+        return DEFAULT_VERSION + SP + OK.code + CRLF + "Content-Length:" + SP +
+                length + CRLF + "Allow:" + SP + "GET, HEAD, OPTIONS" + CRLF +
+                CRLF + body;
     }
 
 
-    public static Request buildRequest(String version, String method, String path, String body, HashMap<String, String> headers){
+    public static Request buildRequest(String version, String method,
+                                       String path, String body,
+                                       HashMap<String, String> headers) {
         return new Request(version, method, path, headers, body);
     }
 
--- src/test/java/httpserver/request/RequestTest.java
+++ src/test/java/httpserver/request/RequestTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RequestTest {
 
@@ -18,48 +18,53 @@ class RequestTest {
     String body = "Test Body";
 
 
-
     @Test
-    @DisplayName("Testing if the request data object will return the appropriate method value")
-    public void testIfRequestObjectReturnCorrectMethodValue(){
-        Request request = TestUtils.buildRequest(version, method, path, body, headers);
+    @DisplayName("Testing if Request returns the appropriate method value")
+    public void testIfRequestObjectReturnCorrectMethodValue() {
+        Request request =
+                TestUtils.buildRequest(version, method, path, body, headers);
         assertEquals(request.method, "GET");
     }
 
     @Test
-    @DisplayName("Testing if the request data object will return the appropriate version value")
-    public void testIfRequestObjectReturnCorrectVersionValue(){
-        Request request = TestUtils.buildRequest(version, method, path, body, headers);
+    @DisplayName("Testing if Request returns the appropriate version value")
+    public void testIfRequestObjectReturnCorrectVersionValue() {
+        Request request =
+                TestUtils.buildRequest(version, method, path, body, headers);
         assertEquals(request.version, "HTTP 1.1");
     }
 
     @Test
-    @DisplayName("Testing if the request data object will return the appropriate path value")
-    public void testIfRequestObjectReturnCorrectPathValue(){
-        Request request = TestUtils.buildRequest(version, method, path, body, headers);
+    @DisplayName("Testing if Request returns the appropriate path value")
+    public void testIfRequestObjectReturnCorrectPathValue() {
+        Request request =
+                TestUtils.buildRequest(version, method, path, body, headers);
         assertEquals(request.path, "/test_path");
     }
 
     @Test
-    @DisplayName("Testing if the request data object will return the appropriate body value")
-    public void testIfRequestObjectReturnCorrectBodyValue(){
-        Request request = TestUtils.buildRequest(version, method, path, body, headers);
+    @DisplayName("Testing if Request returns the appropriate body value")
+    public void testIfRequestObjectReturnCorrectBodyValue() {
+        Request request =
+                TestUtils.buildRequest(version, method, path, body, headers);
         assertEquals(request.body, "Test Body");
     }
 
     @Test
-    @DisplayName("Testing if the request data object will return the appropriate headers value")
-    public void testIfRequestObjectReturnCorrectHeadersValue(){
+    @DisplayName("Testing if Request returns the appropriate headers value")
+    public void testIfRequestObjectReturnCorrectHeadersValue() {
         headers.put("Content-Length", "9");
-        Request request = TestUtils.buildRequest(version, method, path, body, headers);
+        Request request =
+                TestUtils.buildRequest(version, method, path, body, headers);
         assertEquals(request.headers.get("Content-Length"), "9");
     }
 
     @Test
-    @DisplayName("Testing if the request data object will return the appropriate headers key")
-    public void testIfRequestObjectReturnCorrectHeadersKey(){
+    @DisplayName("Testing if Request will return the appropriate headers key")
+    public void testIfRequestObjectReturnCorrectHeadersKey() {
         headers.put("Content-Length", "9");
-        Request request = TestUtils.buildRequest(version, method, path, body, headers);
+        Request request =
+                TestUtils.buildRequest(version, method, path, body, headers);
         assertEquals(request.headers.containsKey("Content-Length"), true);
     }
 
