e3749f0babf58d1f264f9c5af3824247fbd4ac6c
Klaven Jones <klavensjones@gmail.com>
2022-09-19 17:04:48 -0400

    Extracted logic in run() function into a parseMessage function, and added corresponding test

--- src/main/java/httpserver/server/RunnableServer.java
+++ src/main/java/httpserver/server/RunnableServer.java
@@ -8,31 +8,36 @@ import httpserver.response.Response;
 import httpserver.response.ResponseFormatter;
 import httpserver.router.Router;
 
+import java.io.IOException;
+
 
 public class RunnableServer implements Runnable {
     private final ISocket socketWrapper;
-    private RequestParser requestParser;
     private final Router router;
+    private RequestParser requestParser;
 
     public RunnableServer(ISocket socket) {
         this.socketWrapper = socket;
         this.router = new Router();
     }
 
+    public void parseMessage(String clientMessage, ISocket socketWrapper)
+            throws IOException {
+        if (clientMessage != null && clientMessage != "") {
+            requestParser = new RequestParser(clientMessage);
+            Request request = requestParser.parse();
+            Response response = router.handleRequest(request);
+            ResponseFormatter responseFormatter = new ResponseFormatter();
+            socketWrapper.sendData(responseFormatter.formatResponse(response));
+        }
+    }
 
     @Override
     public void run() {
         try {
             String clientMessage = socketWrapper.receiveData();
             System.out.println("Client Connected: " + clientMessage);
-            if (clientMessage != null && clientMessage != "") {
-                requestParser = new RequestParser(clientMessage);
-                Request request = requestParser.parse();
-                Response response = router.handleRequest(request);
-                ResponseFormatter responseFormatter = new ResponseFormatter();
-                socketWrapper.sendData(
-                        responseFormatter.formatResponse(response));
-            }
+            parseMessage(clientMessage, socketWrapper);
             socketWrapper.close();
         } catch (Exception e) {
             System.out.println(e.getMessage());
