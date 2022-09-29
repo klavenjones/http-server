0f5355f74baece1477fa7579bb0baebb44a7f47e
Klaven Jones <klavensjones@gmail.com>
2022-09-29 16:25:59 -0400

    Added acceptance tests, and bash script

--- src/main/java/httpserver/server/HttpServer.java
+++ src/main/java/httpserver/server/HttpServer.java
@@ -8,13 +8,19 @@ import httpserver.wrappers.SocketWrapper;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer {
 
     private static IServerSocket serverSocketWrapper;
+    private static ExecutorService pool;
 
     public static ServerSocket buildServerSocket(int portNumber) {
         serverSocketWrapper = new ServerSocketWrapper();
+        int processingCoresAvailable = Runtime.getRuntime()
+                .availableProcessors();
+        pool = Executors.newFixedThreadPool(processingCoresAvailable);
         return serverSocketWrapper.createServerSocket(portNumber);
     }
 
@@ -26,11 +32,13 @@ public class HttpServer {
                 SocketWrapper socketWrapper = new SocketWrapper(clientSocket);
                 RunnableServer runnableServer =
                         new RunnableServer(socketWrapper);
-                new Thread(runnableServer).start();
+                Thread thread = new Thread(runnableServer);
+                pool.execute(thread);
             }
         } catch (RuntimeException e) {
             throw new IOException(e);
         } finally {
+            System.out.println("Socket Closed");
             serverSocket.close();
         }
     }
